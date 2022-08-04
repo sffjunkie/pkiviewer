07f86e02244dac7a60b5fc1aaf22fe20968e7d23
Simon Kennedy <sffjunkie+code@gmail.com>
2022-08-04 14:31:27 +0000

    Removed unused function

--- src/pkiviewer/asn1/load_specification.py
+++ src/pkiviewer/asn1/load_specification.py
@@ -10,3 +10,11 @@ def load_asn1_specification(spec_name: str) -> asn1tools.compiler.Specification 
         q, codec="der"
     )
     return crl_asn1  # type: ignore
+
+
+def load_future_asn1_specifications(specs: list[str]):
+    p = Path(__file__).parent / "specs" / "future"
+
+    sf = [str((p / f).resolve()) for f in specs]
+    specs = asn1tools.compile_files(sf)
+    return specs
