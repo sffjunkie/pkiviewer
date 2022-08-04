41d21c73d8e9055ff2b31f3f6223eedea2cd156f
Simon Kennedy <sffjunkie+code@gmail.com>
2022-08-04 14:40:36 +0000

    Added skipped test.
    Will be re-enabled if asn1tools get the ability to use
    CLASS statements.

---
+++ tests/integration/test_asn1tools.py
@@ -0,0 +1,10 @@
+import pytest
+
+from pkiviewer.asn1.load_specification import load_asn1_specification
+
+
+@pytest.mark.skip(reason="asn1tools does not support CLASS")
+def test_LoadASN1Grammar():
+    spec_file = "future/PKIX-CommonTypes-2009.asn1"
+
+    spec = load_asn1_specification(spec_file)
