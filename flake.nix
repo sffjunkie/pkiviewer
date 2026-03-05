aea22d50faf0a85c0a91beb99115ffe2b2902d54
Simon Kennedy <sffjunkie+code@gmail.com>
2026-03-05 20:09:54 +0000

    Various structural changes

--- flake.nix
+++ flake.nix
@@ -1,53 +1,106 @@
 {
-  description = "pkiviewer a cedrtificate viewer for your terminal";
+  description = "pkiviewer";
 
-  inputs.pyproject-nix.url = "github:nix-community/pyproject.nix";
-  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  nixConfig = {
-    bash-prompt = ''\n\[\033[1;34m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] '';
-  };
+    pyproject-nix.url = "github:nix-community/pyproject.nix";
+    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = {
-    nixpkgs,
-    pyproject-nix,
-    ...
-  }: let
-    inherit (nixpkgs) lib;
-
-    project = pyproject-nix.lib.project.loadPyproject {
-      projectRoot = ./.;
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ] (system: function nixpkgs.legacyPackages.${system});
-  in {
-    devShells = forAllSystems (pkgs: (
-      let
-        python = pkgs.python3;
-        arg = project.renderers.withPackages {inherit python;};
-        pythonEnv = python.withPackages arg;
-      in {
-        default = pkgs.mkShell {
-          packages = [
-            pkgs.pdm
-            pythonEnv
-          ];
-        };
-      }
-    ));
-
-    packages = forAllSystems (pkgs: (
-      let
-        python = pkgs.python3;
-        attrs = project.renderers.buildPythonPackage {inherit python;};
-      in {
-        default = python.pkgs.buildPythonPackage attrs;
-      }
-    ));
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      uv2nix,
+      pyproject-nix,
+      pyproject-build-systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      forAllSystems = lib.genAttrs [
+        "x86_64-linux"
+      ];
+
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python3;
+
+          pythonBase = pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          };
+
+          pythonSet = pythonBase.overrideScope (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.default
+              overlay
+            ]
+          );
+        in
+        {
+          default = pythonSet.mkVirtualEnv "pkiviewer-env" workspace.deps.default;
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/pkiviewer";
+          meta = {
+            description = "PKI viewer";
+            homepage = "https://github.com/sffjunkie/pkiviewer";
+            license = lib.licenses.asl20;
+          };
+        };
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python3;
+        in
+        {
+
+          default = pkgs.mkShell {
+            packages = [
+              python
+              pkgs.ruff
+              pkgs.just
+              pkgs.pre-commit
+              pkgs.uv
+            ];
+            env = {
+              UV_PYTHON_DOWNLOADS = "never";
+              UV_PYTHON = python.interpreter;
+            };
+          };
+        }
+      );
+    };
 }
