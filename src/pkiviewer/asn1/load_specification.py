ca5afe4b5486ba7c888ef6b74d9e2d5e7a6519d3
Simon Kennedy <sffjunkie+code@gmail.com>
2026-03-15 17:46:18 +0000

    Add CRL test file

--- src/pkiviewer/asn1/load_specification.py
+++ src/pkiviewer/asn1/load_specification.py
@@ -1,21 +1,27 @@
-from pathlib import Path
+# from pathlib import Path
 
-import asn1tools  # type: ignore
+import pyasn1  # type: ignore
+
+pyasn1 = pyasn1
 
 
 def load_asn1_specification(
     spec_name: str,
-) -> asn1tools.compiler.Specification | None:  # type: ignore
-    p = Path(__file__).parent / "specs" / spec_name
-    q = str(p.resolve())
-    crl_asn1: asn1tools.compiler.Specification | None
-    crl_asn1 = asn1tools.compile_files(q, codec="der")  # type: ignore
-    return crl_asn1  # type: ignore
+) -> str | None:
+    # ) -> asn1tools.compiler.Specification | None:  # type: ignore
+    # p = Path(__file__).parent / "specs" / spec_name
+    # q = str(p.resolve())
+    # crl_asn1: asn1tools.compiler.Specification | None = asn1tools.compile_files(
+    #     q, codec="der"
+    # )  # type: ignore
+    # return crl_asn1  # type: ignore
+    return None
 
 
 def load_future_asn1_specifications(specs: list[str]):
-    p = Path(__file__).parent / "specs" / "future"
+    # p = Path(__file__).parent / "specs" / "future"
 
-    sf = [str((p / f).resolve()) for f in specs]
-    specs = asn1tools.compile_files(sf)
+    # sf = [str((p / f).resolve()) for f in specs]
+    # specs = asn1tools.compile_files(sf)
+    specs = []
     return specs
